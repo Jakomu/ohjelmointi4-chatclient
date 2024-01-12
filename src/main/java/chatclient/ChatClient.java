c268236dbee679eedb953c181f591c273caa6ba6
Jakomu <janne.komul@gmail.com>
2024-01-12 02:01:13 +0200

    Change place of config file.

--- src/main/java/chatclient/ChatClient.java
+++ src/main/java/chatclient/ChatClient.java
@@ -37,7 +37,7 @@ public class ChatClient implements ChatClientDataProvider {
 
     public static void main(String[] args) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/main/java/chatclient/config.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("config.txt"));
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("server=")) {
